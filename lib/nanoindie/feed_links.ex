e90b03e9847ff2aa1d75e90bd59be89c0d978e0a
Jose Anasco <joseanasco1@gmail.com>
2018-01-26 15:26:50 -0500

    refactor, get content information trough each entry node

--- lib/nanoindie/feed_links.ex
+++ lib/nanoindie/feed_links.ex
@@ -1,32 +1,52 @@
 defmodule FeedLinks do
   def from_rss(feed_url) do
     feed_url
-    |> rss_contents()
+    |> rss_entries()
+    |> Enum.flat_map(&(Map.get &1, :content))
     |> Enum.flat_map(&get_links/1)
   end
 
-  def rss_contents(feed_url) do
+  def rss_entries(feed_url) do
     feed_url
     |> HTTPoison.get!()
     |> Map.get(:body)
-    |> get_rss_contents()
+    |> parse_rss()
   end
 
-  defp get_rss_contents(body) do
-    parsed_body = Quinn.parse(body)
-
-    get_rss_node_values(parsed_body, :content) ++
-      get_rss_node_values(parsed_body, :"content:encoded")
+  defp parse_rss(body) do
+    body
+    |> Quinn.parse(body)
+    |> get_rss_entry_nodes()
+    |> Enum.map fn(node) ->
+      %{
+        content: get_rss_entry_contents(node)
+      }
+    end
   end
 
-  defp get_rss_node_values(parsed_body, node_name) do
-    parsed_body
-    |> Quinn.find(node_name)
+  defp get_rss_node_values(node, subnode_name) do
+    node
+    |> Quinn.find(subnode_name)
     |> Enum.flat_map fn(quinn_node) ->
       case quinn_node do
         %{value: value} -> value
         _ -> ""
       end
+    end
+  end
+
+  defp get_rss_entry_nodes(main_node) do
+    get_rss_node_values(main_node, :entry) ++
+      get_rss_node_values(main_node, :item)
+  end
+
+  defp get_rss_entry_contents(entry_node) do
+    content_node = get_rss_node_values(entry_node, :content)
+
+    if Enum.empty?(content_node) do
+      get_rss_node_values(entry_node, :"content:encoded")
+    else
+      content_node
     end
   end
 
