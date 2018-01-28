1985bbc15f84683dfbd20496b5093dd36ade0881
Jose Anasco <joseanasco1@gmail.com>
2018-01-28 00:24:35 -0500

    youtube links filter

---
+++ test/nanoindie/youtube_links_filter_test.exs
@@ -0,0 +1,61 @@
+defmodule YoutubeLinksFilterTest do
+  use ExUnit.Case, async: true
+
+  test "returning only embed and watch youtube links" do
+    links  = [
+      "https://www.spotify.com/embed",
+      "https://www.youtube.com/embed/123",
+      "https://www.youtube.com/watch?v=456",
+      "https://www.youtube.com/other/any"
+    ]
+
+    assert Enum.sort(YoutubeLinksFilter.filter links) == [
+      "https://www.youtube.com/watch?v=123",
+      "https://www.youtube.com/watch?v=456"
+    ]
+  end
+
+  test "removing links with repeated video codes, transform into watch links" do
+    links  = [
+      "https://www.youtube.com/embed/123",
+      "https://www.youtube.com/watch?v=123",
+      "https://www.youtube.com/watch?v=456",
+      "https://www.youtube.com/embed/456",
+      "https://www.youtube.com/embed/679",
+    ]
+
+    assert Enum.sort(YoutubeLinksFilter.filter links) == [
+      "https://www.youtube.com/watch?v=123",
+      "https://www.youtube.com/watch?v=456",
+      "https://www.youtube.com/watch?v=679",
+    ]
+  end
+
+  test "removing not needed params" do
+    links = [
+      "https://www.youtube.com/watch?v=444&feature=oembed&other=other",
+      "https://www.youtube.com/watch?v=123",
+      "https://www.youtube.com/embed/456?feature=oembed&other=other",
+      "https://www.youtube.com/embed/789?rel=oembed?some"
+    ]
+
+    assert Enum.sort(YoutubeLinksFilter.filter links) == [
+      "https://www.youtube.com/watch?v=123",
+      "https://www.youtube.com/watch?v=444",
+      "https://www.youtube.com/watch?v=456",
+      "https://www.youtube.com/watch?v=789"
+    ]
+  end
+
+  test "removing list urls" do
+    links = [
+      "https://www.youtube.com/embed/789&list=123",
+      "https://www.youtube.com/watch?v=789",
+      "https://www.youtube.com/watch?v=456&list=some",
+    ]
+
+    assert Enum.sort(YoutubeLinksFilter.filter links) == [
+      "https://www.youtube.com/watch?v=789"
+    ]
+  end
+end
