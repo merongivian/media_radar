87f852645cfd1c032a8685b76d7cc400ef5ec8ef
Jose Anasco <joseanasco1@gmail.com>
2018-02-17 18:52:30 -0500

    limit number of songs shown

--- lib/nanoindie_web/controllers/song_controller.ex
+++ lib/nanoindie_web/controllers/song_controller.ex
@@ -1,13 +1,18 @@
 defmodule NanoindieWeb.SongController do
   alias Nanoindie.{Song, Blog, Repo}
   use NanoindieWeb, :controller
+  require Ecto.Query
 
   def index(conn, params) do
     songs = if params["blog_id"] do
-              Blog |> Repo.get_by(id: params["blog_id"]) |> Ecto.assoc(:songs) |> Repo.all()
+              Blog
+              |> Repo.get_by(id: params["blog_id"])
+              |> Ecto.assoc(:songs)
             else
-              Repo.all(Song)
+              Song
             end
+            |> Ecto.Query.limit(200)
+            |> Repo.all()
 
     render conn, "index.html", songs: songs
   end
