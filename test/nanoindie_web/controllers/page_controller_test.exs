557886e762c1d76f7ab12fe7dd756b5a08f08f87
Jose Anasco <joseanasco1@gmail.com>
2018-03-08 10:50:25 -0500

    remove generated test

--- test/nanoindie_web/controllers/page_controller_test.exs
+++ test/nanoindie_web/controllers/page_controller_test.exs
@@ -1,8 +1,4 @@
 defmodule NanoindieWeb.PageControllerTest do
   use NanoindieWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
 end
